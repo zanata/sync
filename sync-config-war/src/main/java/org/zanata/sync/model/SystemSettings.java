9ac111f5dbc9ccc3f355cbaa76c2ddfa4a2c80c9
Patrick Huang <pahuang@redhat.com>
2016-06-22 16:32:00 +1000

    WIP change to use mysql

--- sync-config-war/src/main/java/org/zanata/sync/model/SystemSettings.java
+++ sync-config-war/src/main/java/org/zanata/sync/model/SystemSettings.java
@@ -1,20 +1,34 @@
+/*
+ * Copyright 2016, Red Hat, Inc. and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.zanata.sync.model;
 
 import java.io.Serializable;
-import java.util.List;
 import javax.persistence.Access;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-import javax.persistence.UniqueConstraint;
 import javax.persistence.Version;
-import javax.validation.constraints.NotNull;
-
-import org.hibernate.validator.constraints.NotEmpty;
-import com.google.common.collect.Lists;
 
 import static javax.persistence.AccessType.FIELD;
 
@@ -34,8 +48,10 @@ public class SystemSettings implements Serializable {
     @Version
     private int version;
 
+    @Column(name = "settingKey")
     private String key;
 
+    @Column(name = "settingValue")
     private String value;
 
     public String getKey() {
